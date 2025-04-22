f9fdc7af26325fb538193e8cdc3892e9a02c3b0c
debajit13 <mallickdebajit3@gmail.com>
2025-04-22 20:40:32 +0530

    remove unused video

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import HeroTwo from './components/HeroTwo';
 import AISelling from './components/AISelling';
 import Carousel from './components/Carousel';
 import BuyerFeatures from './components/BuyerFeatures';
@@ -10,10 +9,6 @@ export default function Home() {
   return (
     <div className='relative bg-white min-h-full flex flex-col items-center justify-center text-center pt-1 xl:pt-0  pb-6 pr-0 overflow-hidden'>
       <HeroSection />
-
-      {/* <div className='block xl:hidden'>
-        <HeroTwo />
-      </div>  */}
       <AISelling />
       <Carousel
         carouselContent={carouselContent}
